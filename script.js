8da9187f60aaef2bbf0f1954b9b19ee6618f5130
Shreyas <121279572+Shreyasssonar@users.noreply.github.com>
2023-08-26 09:19:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function delayAndResolve() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000); // 1000 milliseconds = 1 second
+  });
+}
+
+async function updateElementWithText() {
+  const outputElement = document.getElementById("output");
+
+  try {
+    const result = await delayAndResolve();
+    outputElement.textContent = result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+updateElementWithText();
